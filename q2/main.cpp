7997d86ee89a68e98a620be4be02ab9632eb0f5f
Mike Chaberski <mac937@nyu.edu>
2020-02-28 21:41:00 -0500

    q2: implemented

--- q2/main.cpp
+++ q2/main.cpp
@@ -2,10 +2,42 @@
 // hw8 question 2
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+// Converts simple lower case letters to upper case.
+// Only modifies characters in the range [a, z]. 
+// Returns any other argument unchanged.
+char toUpperCase(char ch) {
+    if (ch >= 'a' && ch <= 'z') {
+        return static_cast<char>(ch - ('a' - 'A'));
+    }
+    return ch;
+}
+
+// Checks whether a string is a palindrome, case-insensitively
+// Behavior may not be what you want if string contains non-letters such 
+// as spaces or apostrophes, or letters with diacritic marks.
+bool isPalindrome(const string& str) {
+    int length = str.length();
+    for (int i = 0; i < length / 2; i++) {
+        if (toUpperCase(str[i]) != toUpperCase(str[length - i - 1])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Asks for single word and reports whether it's a palindrome.
 int main() {
-    cout << "q2 executed" << endl;
+    string word;
+    cout << "Please enter a word: ";
+    cin >> word;
+    cout << word << " is ";
+    if (!isPalindrome(word)) {
+        cout << "not ";
+    }
+    cout << "a palindrome" << endl;
     return 0;
 }
--- q2/question.md
+++ q2/question.md
@@ -1,3 +1,16 @@
 # Question 2
 
-Write a program...
+A *palindrome* is a word, which reads the same backward or forward. For 
+example, noon, civic, radar, level, rotor, kayak, reviver, racecar, redder, 
+madam, and refer are all palindromes.
+
+a. Implement a function `bool isPalindrome(string str)` that, given a string 
+  `str` containing a word, returns true if and only if `str` is a palindrome.
+  
+b. Write a program that reads a word from the user and announces to the user 
+   if it is a palindrome or not.
+
+Your program should interact with the user exactly as it shows in the following example:
+
+    Please enter a word: level
+    level is a palindrome
